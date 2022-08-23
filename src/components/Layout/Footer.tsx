835472ce453323ae350fd0b41c948fc06cc63432
Joan Gavelán <joangavelan@gmail.com>
2022-08-23 12:16:48 -0500

    change color

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className='text-center text-sm font-semibold text-slate-800 md:text-base'>
+    <footer className='text-center text-sm font-semibold text-gray-600 md:text-base'>
       Built with Next.js, Tailwind CSS, Headless UI, React Beautiful DND and
       more!
     </footer>
