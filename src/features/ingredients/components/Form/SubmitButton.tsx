70086b0537aa4994ccf095f3453c6242e3414d14
Joan Gavelán <joangavelan@gmail.com>
2022-08-28 10:38:10 -0500

    add disabled and hover styles

--- src/features/ingredients/components/Form/SubmitButton.tsx
+++ src/features/ingredients/components/Form/SubmitButton.tsx
@@ -4,8 +4,9 @@ export const SubmitButton = () => {
   return (
     <div>
       <button
+        disabled={false}
         type='submit'
-        className='h-full rounded-lg border border-lime-300 bg-lime-200 px-2.5 py-1.5 text-lime-700 sm:px-4 sm:py-2'
+        className='h-full rounded-lg border border-lime-300 bg-lime-200 px-2.5 py-1.5 text-lime-700 transition duration-200 enabled:hover:bg-lime-400 disabled:border-gray-300 disabled:bg-gray-300 disabled:text-gray-500 sm:px-4 sm:py-2'
       >
         <FaPlus />
       </button>
