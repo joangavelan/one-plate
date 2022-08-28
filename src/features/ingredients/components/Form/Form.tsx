30f99f177224cb45200f77da21b2711d04ac4478
Joan Gavelán <joangavelan@gmail.com>
2022-08-28 16:59:09 -0500

    implement form validation

--- src/features/ingredients/components/Form/Form.tsx
+++ src/features/ingredients/components/Form/Form.tsx
@@ -4,51 +4,47 @@ import { Unit } from './Unit'
 import { SubmitButton } from './SubmitButton'
 import { Amount } from './Amount'
 import { useForm, SubmitHandler } from 'react-hook-form'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-
-const schema = z.object({
-  searchQuery: z.string().trim().min(1),
-  amount: z.number(),
-  unit: z.string()
-})
 
 export type FormData = {
-  searchQuery: string
   amount: number
   unit: string
 }
 
+export type SelectedIngredient = {
+  id: number
+  name: string
+}
+
 export const IngredientForm = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    control,
-    formState: { errors }
-  } = useForm<FormData>({
-    resolver: zodResolver(schema)
-  })
-  const [selectedIngredient, setSelectedIngredient] = React.useState('')
-  const [possibleUnits, setPossibleUnits] = React.useState([])
+  const { register, handleSubmit, watch, control } = useForm<FormData>()
 
-  const query = watch('searchQuery')
+  const [selectedIngredient, setSelectedIngredient] =
+    React.useState<SelectedIngredient | null>(null)
+  const [possibleUnits, setPossibleUnits] = React.useState(['g'])
 
-  const onSubmit: SubmitHandler<FormData> = (data) => {
-    console.log(data)
+  const onSubmit: SubmitHandler<FormData> = ({ amount, unit }) => {
+    const ingredient = {
+      id: selectedIngredient?.id,
+      name: selectedIngredient?.name,
+      amount,
+      unit
+    }
+    console.log(ingredient)
   }
+
+  const amount = watch('amount')
+  const unit = watch('unit')
+  const isReadyToSubmit = selectedIngredient && amount && unit
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className='my-5 flex gap-3'>
       <IngredientSearch
-        registration={register('searchQuery')}
-        query={query}
         selected={selectedIngredient}
         setSelected={setSelectedIngredient}
       />
       <Amount registration={register('amount', { valueAsNumber: true })} />
       <Unit units={possibleUnits} control={control} name='unit' />
-      <SubmitButton />
+      <SubmitButton disabled={!isReadyToSubmit} />
     </form>
   )
 }
--- src/features/ingredients/components/Form/SubmitButton.tsx
+++ src/features/ingredients/components/Form/SubmitButton.tsx
@@ -1,12 +1,16 @@
 import { FaPlus } from 'react-icons/fa'
 
-export const SubmitButton = () => {
+type SubmitButtonProps = {
+  disabled: boolean
+}
+
+export const SubmitButton = ({ disabled }: SubmitButtonProps) => {
   return (
     <div>
       <button
-        disabled={false}
+        disabled={disabled}
         type='submit'
-        className='h-full rounded-lg border border-lime-300 bg-lime-200 px-2.5 py-1.5 text-lime-700 transition duration-200 enabled:hover:bg-lime-400 disabled:border-gray-300 disabled:bg-gray-300 disabled:text-gray-500 sm:px-4 sm:py-2'
+        className='h-full rounded-lg border border-lime-300 bg-lime-200 px-2.5 py-1.5 text-lime-700 enabled:hover:bg-lime-400 disabled:border-gray-300 disabled:bg-gray-300 disabled:text-gray-500 sm:px-4 sm:py-2'
       >
         <FaPlus />
       </button>
