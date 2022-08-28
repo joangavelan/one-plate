c32f864ab1f3fe0d341fc9dd4176c48441e9183f
Joan Gavelán <joangavelan@gmail.com>
2022-08-27 19:54:59 -0500

    search input registered and controlled

--- src/features/ingredients/components/Form/Form.tsx
+++ src/features/ingredients/components/Form/Form.tsx
@@ -3,11 +3,32 @@ import { IngredientSearch } from './Search'
 import { Unit } from './Unit'
 import { SubmitButton } from './SubmitButton'
 import { Amount } from './Amount'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+type FormData = {
+  searchQuery: string
+  amount: number
+  unit: string
+}
 
 export const IngredientForm = () => {
+  const { register, handleSubmit, watch } = useForm<FormData>()
+  const [selectedIngredient, setSelectedIngredient] = React.useState('')
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data)
+  }
+
+  const query = watch('searchQuery')
+
   return (
-    <form className='my-5 flex gap-3'>
-      <IngredientSearch />
+    <form onSubmit={handleSubmit(onSubmit)} className='my-5 flex gap-3'>
+      <IngredientSearch
+        registration={register('searchQuery')}
+        query={query}
+        selected={selectedIngredient}
+        setSelected={setSelectedIngredient}
+      />
       <Amount />
       <Unit />
       <SubmitButton />
--- src/features/ingredients/components/Form/Search.tsx
+++ src/features/ingredients/components/Form/Search.tsx
@@ -1,16 +1,26 @@
-import { Fragment, useState } from 'react'
+import * as React from 'react'
 import { Combobox, Transition } from '@headlessui/react'
 import ingredients from '@/data/ingredients.json'
+import { UseFormRegisterReturn } from 'react-hook-form'
 
 type Ingredient = {
   id: number
   name: string
 }
 
-export const IngredientSearch = () => {
-  const [selected, setSelected] = useState('')
-  const [query, setQuery] = useState('')
+type IngredientSearchProps = {
+  registration: UseFormRegisterReturn
+  query: string
+  selected: string
+  setSelected: React.Dispatch<React.SetStateAction<string>>
+}
 
+export const IngredientSearch = ({
+  query,
+  registration,
+  selected,
+  setSelected
+}: IngredientSearchProps) => {
   const filteredIngredients =
     query === ''
       ? ingredients.slice(0, 10)
@@ -19,7 +29,7 @@ export const IngredientSearch = () => {
             ingredient.name
               .toLowerCase()
               .replace(/\s+/g, '')
-              .includes(query.toLowerCase().replace(/\s+/g, ''))
+              .includes(query?.toLowerCase().replace(/\s+/g, ''))
           )
           .slice(0, 10)
 
@@ -30,17 +40,17 @@ export const IngredientSearch = () => {
         <Combobox.Input
           className='inputStyles leading-5 text-gray-900'
           displayValue={(ingredient: Ingredient) => ingredient?.name}
-          onChange={(event) => setQuery(event.target.value)}
           placeholder='Apple'
           autoComplete='off'
+          {...registration}
         />
 
         <Transition
-          as={Fragment}
+          as={React.Fragment}
           leave='transition ease-in duration-100'
           leaveFrom='opacity-100'
           leaveTo='opacity-0'
-          afterLeave={() => setQuery('')}
+          // afterLeave={() => setQuery('')}
         >
           {/* options */}
           <Combobox.Options className='absolute z-10 mt-2 max-h-40 w-full min-w-min overflow-auto rounded-md bg-white py-1 ring-1 ring-black ring-opacity-5 focus:outline-none sm:max-h-52 sm:text-sm lg:max-h-60'>
