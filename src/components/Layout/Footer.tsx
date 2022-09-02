c209edcec11166c420d9b2f5e0a09aa6c7ec3d08
Joan Gavelán <joangavelan@gmail.com>
2022-09-02 11:54:02 -0500

    update technologies in footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,8 +1,39 @@
+const technologies = [
+  {
+    name: 'Headless UI',
+    href: 'https://headlessui.com/'
+  },
+  {
+    name: 'Tailwind CSS',
+    href: 'https://tailwindcss.com/'
+  },
+  {
+    name: 'React Beautiful DND',
+    href: 'https://github.com/atlassian/react-beautiful-dnd'
+  },
+  {
+    name: 'Chart.js',
+    href: 'https://www.chartjs.org/'
+  }
+]
+
 export const Footer = () => {
   return (
-    <footer className='text-center text-sm font-semibold text-gray-600 md:text-base'>
-      Built with Next.js, Tailwind CSS, Headless UI, React Beautiful DND and
-      more!
+    <footer className='text-center text-sm font-semibold text-gray-400 md:text-base'>
+      Built with{' '}
+      {technologies.map(({ name, href }) => (
+        <span key={name} className='[&:not(:last-child)]:mr-1'>
+          <a
+            href={href}
+            target='_blank'
+            rel='noreferrer noopener'
+            className='text-gray-700 hover:underline'
+          >
+            {name},
+          </a>
+        </span>
+      ))}{' '}
+      and more!
     </footer>
   )
 }
