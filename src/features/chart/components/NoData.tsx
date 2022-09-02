a39e18637dfbcc1c8d72dd27bcbd29c0c7f9f741
Joan Gavelán <joangavelan@gmail.com>
2022-09-02 12:07:22 -0500

    small improvements to empty chart responsiveness

--- src/features/chart/components/NoData.tsx
+++ src/features/chart/components/NoData.tsx
@@ -2,10 +2,10 @@ import { BsFillPieChartFill } from 'react-icons/bs'
 
 export const NoData = () => {
   return (
-    <div className='green-box col-span-5 grid place-items-center gap-4 text-gray-400 lg:col-span-2'>
-      <div className='my-5 flex flex-col items-center gap-3'>
-        <BsFillPieChartFill className='h-32 w-32 md:h-40 md:w-40' />
-        <p className='text-lg font-medium'>No data available</p>
+    <div className='green-box col-span-5 grid min-h-[20rem] place-items-center text-gray-400 lg:col-span-2'>
+      <div className='flex flex-col items-center gap-4'>
+        <BsFillPieChartFill className='h-28 w-28 lg:h-40 lg:w-40' />
+        <p className='text-base font-medium lg:text-lg'>No data available</p>
       </div>
     </div>
   )
