92398fada8715dc74ece4bfeca10ab4e2962a91f
Joan Gavelán <joangavelan@gmail.com>
2022-11-21 19:29:45 -0500

    fix chart datasets

--- src/features/chart/components/Chart.tsx
+++ src/features/chart/components/Chart.tsx
@@ -23,6 +23,8 @@ export const Chart = () => {
     [ingredients]
   )
 
+  console.log(totalAmountsPerNutrient)
+
   const totalCalories = React.useMemo(
     () =>
       ingredients.reduce((acc, ing) => {
@@ -33,19 +35,19 @@ export const Chart = () => {
 
   const data: ChartData<'pie'> = React.useMemo(
     () => ({
-      labels: ['Carbohydrates', 'Proteins', 'Fats'],
+      labels: ['Fats', 'Proteins', 'Carbohydrates'],
       datasets: [
         {
           data: totalAmountsPerNutrient,
           backgroundColor: [
-            'rgba(54, 162, 235, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
             'rgba(255, 159, 64, 0.2)',
-            'rgba(75, 192, 192, 0.2)'
+            'rgba(54, 162, 235, 0.2)'
           ],
           borderColor: [
-            'rgba(54, 162, 235, 1)',
+            'rgba(75, 192, 192, 1)',
             'rgba(255, 159, 64, 1)',
-            'rgba(75, 192, 192, 1)'
+            'rgba(54, 162, 235, 1)'
           ],
           borderWidth: 1
         }
