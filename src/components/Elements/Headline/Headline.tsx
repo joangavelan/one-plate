e7b525dbd84b7216258cb65457982eff4feba42a
Joan Gavelán <joangavelan@gmail.com>
2022-11-28 18:36:49 -0500

    add dot at the end of the line

--- src/components/Elements/Headline/Headline.tsx
+++ src/components/Elements/Headline/Headline.tsx
@@ -2,7 +2,7 @@ export const Headline = () => {
   return (
     <div className='flex text-base sm:text-lg md:text-xl lg:text-2xl'>
       <h1 className='mr-1.5 font-extrabold text-green-600'>One Plate.</h1>
-      <p className='font-medium text-slate-900'>Craft your perfect meal</p>
+      <p className='font-medium text-slate-900'>Craft your perfect meal.</p>
     </div>
   )
 }
