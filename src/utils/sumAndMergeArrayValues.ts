f8becc48dcb8a860f8b106f26552141014aec018
Joan Gavelán <joangavelan@gmail.com>
2022-12-19 09:38:19 -0500

    math round each element of the sum to prevent variations in the result

--- src/utils/sumAndMergeArrayValues.ts
+++ src/utils/sumAndMergeArrayValues.ts
@@ -1,2 +1,2 @@
 export const sumAndMergeArrayValues = (a1: number[], a2: number[]): number[] =>
-  a1.length === a2.length ? a1.map((v, i) => Math.round(v + a2[i]!)) : a2
+  a1.length === a2.length ? a1.map((v, i) => Math.round(v) + Math.round(a2[i]!)) : a2
